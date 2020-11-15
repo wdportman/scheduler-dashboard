0e99cd53634491a0f091c83b5e8eec527c5ec7db
Will <wdportman@gmail.com>
2020-11-14 17:36:17 -0800

    Add Loading and Panel components

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,11 +2,73 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 
-class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
+import Loading from './Loading.js';
+import Panel from './Panel';
 
-    return <main className={dashboardClasses} />;
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
+
+class Dashboard extends Component {
+  state = {
+    loading: false,
+    focused: null
+  };
+
+  selectPanel(id) {
+    this.setState({
+     focused: id
+    });
+   };
+
+  render() {
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
+    const panels = data
+    .filter(
+      panel => this.state.focused === null || this.state.focused === panel.id
+    )
+    .map(row => {
+      const {id, label, value} = row;
+      return(
+        <Panel
+          key={id}
+          id={id}
+          label={label}
+          value={value}
+        />)
+    });
+
+    return(
+      <main className={dashboardClasses}>
+        {panels}
+      </main>
+    );
   }
 }
 
