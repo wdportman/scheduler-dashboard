5b95bb02568c520d0f995931f7d5ff60cde0c479
Will <wdportman@gmail.com>
2020-11-16 22:04:38 -0800

    Set local storage to whichever panel is focused

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -30,14 +30,31 @@ const data = [
 
 class Dashboard extends Component {
   state = {
-    loading: false,
-    focused: null
+    loading: true,
+    focused: null,
+    days: [],
+    appointments: {},
+    interviewers: {}
   };
 
   selectPanel(id) {
     this.setState(previousState => ({
       focused: previousState.focused !== null ? null : id
     }));
+  };
+
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({ focused });
+    }
+  };
+
+  componentDidUpdate(previousProps, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
   };
 
   render() {
